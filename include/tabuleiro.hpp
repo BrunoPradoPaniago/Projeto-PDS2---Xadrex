c6973b15ec11502da64e0871fd594d5117af4080
daviclark3312 <knxmito@gmail.com>
2026-05-12 11:45:46 -0300

    Tabuleiro.h
    Fazendo o tabuleiro .h com o que o professor disse hoje, utilizando uma matriz de ponteiros

--- include/tabuleiro.hpp
+++ include/tabuleiro.hpp
@@ -1,3 +1,6 @@
+#ifndef TABULEIRO_H
+#define TABULEIRO_H
+
 #include<vector>
 #include<pecas.hpp>
 
@@ -5,16 +8,15 @@ using std::vector;
 
 class tabuleiro{
     public:
+vector<vector<pecas*>>matriz; //matriz de ponteiros inteiros para classe peça
     //Construtor de tabuleiro. Aloca a memoria das pecas
     // e cria a matriz ja com as pecas no lugar certo. 
     tabuleiro();
 
     //destrutor de tabuleiro, desaloca a memoria das pecas.
     ~tabuleiro();
-
+    bool moverpeca(int x1, int y1, int x2, int y2);
     bool estaEmXeque(int equipe);
-
     bool estaEmMate(int equipe);
-
-    vector<vector<peca*>> matriz{8, vector<peca*>(8, nullptr)};
 };
+#endif
