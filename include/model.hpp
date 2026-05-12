c70a9e5e5b00456a73061b8d524546c7de981274
daviclark3312 <knxmito@gmail.com>
2026-05-12 12:00:42 -0300

    Criando classe model
    Add initial structure for Model class in model.hpp

---
+++ include/model.hpp
@@ -0,0 +1,13 @@
+//Clase modelo 
+//Colocar turno, checar condiçoes especiais (mate e roque?), verifica se é a vez do jogador 
+
+#infndef MODELO_HPP
+#define MODELO_HPP
+
+#include "tabulero.hpp"
+
+Class Model {
+private:
+
+public: //construtor para iniciar turno 
+
